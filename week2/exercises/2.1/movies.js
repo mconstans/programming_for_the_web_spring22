0b6fc254db6fc5d56e6d618e96d46d0c720cf884
mconstans <82624128+mconstans@users.noreply.github.com>
2022-01-26 22:35:18 -0800

    week 2 folder creation + upload of first exercise

---
+++ week2/exercises/2.1/movies.js
@@ -0,0 +1,5 @@
+let myfavMovies = ["Jurassic Park", "ET", "The Secret Garden", "Free Guy", "My Neighbor Totoro"];
+const userfavMovie = window.prompt('What is your favorite movie?');
+myfavMovies.push(userfavMovie);
+console.log(myfavMovies);
+const ourfavMovies = window.alert (myfavMovies.toString());
