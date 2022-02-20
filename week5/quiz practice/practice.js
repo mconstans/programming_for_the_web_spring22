4d0d680e5f27dfbb2a5dfcfd22f30b1d5333df3e
mconstans <82624128+mconstans@users.noreply.github.com>
2022-02-20 13:09:13 -0800

    quiz practice

---
+++ week5/quiz practice/practice.js
@@ -0,0 +1,10 @@
+// const myDogs = ['Lucky', 'Lucy', 'Linda', 'Lacey', 'Carl'];
+// for (let i = 0; i < myDogs.length; i++) {
+//     console.log('My dog ' + myDogs[i] + ' is a really good puppy.');
+// }
+
+
+let greekMythicalCharacters = ["Athena", "Zeus", "Hermes", "Hercules", "Hera", "that one guy"];
+greekMythicalCharacters.pop();
+greekMythicalCharacters.push('Hades');
+console.log(greekMythicalCharacters);
