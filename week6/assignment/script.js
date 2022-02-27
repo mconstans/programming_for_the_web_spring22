6b4599a0d323a4e28cd73f889f45eed232ca132d
mconstans <82624128+mconstans@users.noreply.github.com>
2022-02-26 23:31:03 -0800

    week 6 folder upload

---
+++ week6/assignment/script.js
@@ -0,0 +1,18 @@
+//new instance of the Vue class
+new Vue({
+    el: "#myApp",
+    data: {
+      message: "No Probllama, Facts from a Llama!",
+      llamaFacts: "Top 10 Funky Facts about those crazy Llamas",
+      llamaBite: "Llamas don't bite. They spit when they're agitated, but that's mostly at each other.",
+      llamaSpit: "Llama spit is green due to undigested food",
+      llamaTherapy: "Like dogs, llamas can be trained as therapy animals to work in hospitals and schools. Many say llamas have a 'soothing aura.'",
+      llamaPoop: "Llama farmers refer to llama manure as 'llama beans'",
+      llamaBabies: "Mama llamas usually only have one baby at a time and llama twins are incredibly rare. Pregnancy lasts for about 350 days, nearly a full year.",
+      llamaGuard: "Llamas have been used as guard animals for livestock like sheep or even alpacas in North America since the '80s. They require almost no training to be an effective guard.",
+      llamaWeight: "Llamas know their own limits. If you try to overload a llama with too much weight, the llama is likely to lie down or simply refuse to move.",
+      llamaPecking: "Like chickens, llamas have a pecking order",
+      llamaSmell: true,
+      llamaGroups: true,
+    }   
+  });
---
+++ week6/exercises/6.2/script.js
@@ -0,0 +1,10 @@
+//new instance of the Vue class
+new Vue({
+    el: "#myApp",
+    data: {
+      message: "Hello World!",
+      vueIsAwesome: true,
+      skyIsBlue: true
+    }
+  });
+  
